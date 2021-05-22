f775301350b61cfb246c27fb2f7f7460e4bcab98
Namrata Patel <56909885+Nam015@users.noreply.github.com>
2021-05-22 18:02:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+  function transform() {
+    document.getElementById('robo').setAttribute('class', 'robo_fadeIn');
+    document
+      .getElementById('circleOuter')
+      .setAttribute('class', 'circle_out_clicked');
+    document
+      .getElementById('circleOuterDashed')
+      .setAttribute('class', 'circle_out_dashed_clicked');
+    document
+      .getElementById('circleInner')
+      .setAttribute('class', 'circle_in_clicked');
+    document.getElementById('logo').setAttribute('class', 'imgSize_clicked');
+    document
+      .getElementById('clickAbove')
+      .setAttribute('class', 'textFadeOut_clicked');
+    document
+      .getElementById('welcome')
+      .setAttribute('class', 'textFadeIn_clicked');
+  }
